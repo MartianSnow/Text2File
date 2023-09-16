0415797c984ade5c58250d01cb44ff6bfa5f51ca
MartianSnow <vlad_kabachenko@ukr.net>
2023-09-16 16:27:42 +0300

    32155

--- CSV/src/Main.java
+++ CSV/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,23 +9,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String csv = "C:\\Users\\Vlad\\OneDrive\\Desktop\\TestFile.csv";
-        Reader r = new Reader();
-        r.CSV(csv);
-    }
-
-    static class Reader {
-        public void CSV(String csv) {
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(csv));
-                while((line = br.readLine()) != null){
-                    String[] occupation = line.splitby(csvSplitBy);
-                    System.out.println("Occupation " + occupation[3]);
-                }
-            }
-            catch (IOException e){
-                e.printStackTrace();
-            }
+        String[] cars = {"BMW", "Volvo", "Toyota", "VW"};
+        for(int i = 0; i < cars.length; i++){
+            System.out.println("The car make is: "+ cars[i]);
         }
     }
 }
