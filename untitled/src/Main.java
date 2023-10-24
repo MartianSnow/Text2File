5e2ee0c73d0000e5b3dfa73aacbfabf56bb5652d
MartianSnow <vlad_kabachenko@ukr.net>
2023-10-24 22:57:06 +0300

    Version3

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,4 @@
-import java.io.*;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,20 +11,5 @@ public class Main {
         String text = sc.nextLine();
 
         wrt.WriteToFile(txt, text);
-    }
-    static class WriteToFile {
-        public void WriteToFile(String path, String text) {
-            File file = new File(path);
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-            Date date = new Date();
-            try{
-                BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
-                writer.write("\n" + formatter.format(date) + ": " + text + " !");
-                writer.close();
-            }
-            catch(IOException e){
-                e.printStackTrace();
-            }
-        }
     }
 }
---
+++ untitled/src/WriteToFile.java
@@ -0,0 +1,22 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class WriteToFile {
+    public void WriteToFile(String path, String text) {
+        File file = new File(path);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+            writer.write("\n" + formatter.format(date) + ": " + text + " !");
+            writer.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
